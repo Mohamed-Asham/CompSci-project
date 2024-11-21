667f83c732ac4c07898a272ef55fbc2afd221b6d
monedj <mnedjadi2003@gmail.com>
2024-11-21 09:55:37 +0000

    Few changes

--- COMBINED CODE.py
+++ COMBINED CODE.py
@@ -345,199 +345,225 @@ def delete_accounts():
 
 #====================Patient Homepage======================
 def update_account_page(email_address):
-    data= load_accounts()
-    account_details= data["patient"][email_address]
-    print("These are your current account details:\n")
-    counter=1
-    for key,value in account_details.items():
-        if key != "password":
-            print("[",counter,"] ",(key[0].upper())+key[1:len(key)].replace("_"," "),": ",value)
-            counter+=1
-    print("\n")
-    update_option = int(input("Please select a option to edit:"))
+    data = load_accounts()
+    account_details = data["patient"][email_address]
+    updates_made = False
 
-    if update_option == 1:
-        while True:
-            new_email= input("Please enter your new email address:").strip()
-            email_pattern = r"^[\w\.-]+@[\w\.-]+\.[\w\.-]+$"
-            email_check = checking_email(new_email)
+    while True:
+        print("--------------------------------------------------------------------------------")
+        print("These are your current account details:\n")
+        counter = 1
+        for key, value in account_details.items():
+            if key != "password":
+                print("[", counter, "] ", (key[0].upper()) + key[1:len(key)].replace("_", " "), ": ", value)
+                counter += 1
+        print("[ 0 ]  Exit to Homepage ")
+        print("\n")
 
-            if email_check == "Email already registered":
-                print("This email is already registered. Please use a different email.")
+
+        update_option = input("Please select a option to edit: ").strip()
+        if (update_option.isdigit() and 0 <= int(update_option) <= 9) :
+            update_option = int(update_option)
+        else:
+            print("\n")
+            print("Invalid input. Please choose a valid option")
+            continue
+
+
+        if update_option == 0:
+            print("Returning to your homepage...")
+            sleep(1)
+            patients_page(email_address)
+            break
+
+
+        if update_option == 1:
+            while True:
+                new_email = input("Please enter your new email address:").strip()
+                email_pattern = r"^[\w\.-]+@[\w\.-]+\.[\w\.-]+$"
+
+                if new_email == email_address:
+                    print("This is already your current email. ")
+                    break
+
+                if new_email in data["patient"] and new_email != email_address:
+                    print("This is already your current email. No changes made. ")
+                    break
+
+
+                elif re.match(email_pattern,new_email):
+                    account_details["email"] = new_email
+                    del data["patient"][email_address]
+                    data["patient"][new_email] = account_details
+                    email_address= new_email
+                    updates_made=True
+                    break
+                else:
+                    print("Invalid email format. Please enter a valid email.")
+
+        elif update_option == 2:
+            new_first_name = input("Please enter your new first name: ").strip()
+            if new_first_name != account_details["name"]:
+                account_details["name"] = new_first_name
+                updates_made= True
             else:
-                if match(email_pattern, new_email):
-                    break
-                elif not match(email_pattern, new_email):
-                    print("Invalid email format. Please enter a valid email.")
+                print("This is already your current first name")
+
+
+
+        elif update_option == 3:
+            new_surname = input("Please enter your new surname").strip()
+            if new_surname != account_details["surname"]:
+                account_details["surname"] = new_surname
+                updates_made= True
+            else:
+                print("This is already your current surname")
+
+
+        elif update_option == 4:
+            while True:
+                new_birthday = input("Birth date (DD/MM/YYYY): ").strip()  # Pad single digit day with zero if necessary
+                if new_birthday != account_details["date_of_birth"]:
+                    try:
+                        day, month, year = map(int, new_birthday.split("/"))
+                        datetime(year,month,day)
+                        account_details["date_of_birth"] = new_birthday
+                        updates_made= True
+                        break
+                    except ValueError:
+                        print("Invalid birth date. Please enter in the format DD/MM/YYYY.")
                 else:
-                    print("Invalid email format. Please enter a valid email.")
-        account_details["email"]= new_email
-        del data["patient"][email_address]
-        data["patient"][new_email]= account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file ,indent=4)
-        print("Your email has been updated successfully.")
-        patients_page(new_email)
-
-    elif update_option ==2:
-        new_first_name = input("Please enter your new first name: ").strip()
-        account_details["name"]= new_first_name
-        data["patient"][email_address]= account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your name has been upodated successfully")
-        patients_page(email_address)
-
-    elif update_option == 3:
-        new_surname = input("Please enter your new surname").strip()
-        account_details["surname"]= new_surname
-        data["patient"][email_address]= account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your surname has been upodated successfully")
-        patients_page(email_address)
-
-    elif update_option == 4:
-        while True:
-            new_birthday = input("Birth date (DD/MM/YYYY): ").strip()  # Pad single digit day with zero if necessary
-            try:
-                new_birthday.zfill(2)
-                day, month, year = new_birthday.split("/")
-                day = int(day)
-                month = int(month)
-                year = int(year)
-            except Exception:
-                print(f"Invalid birth date, try again.")
-                continue
-            try:
-                if not (1 <= day <= 31):
-                    print("Invalid day. Please enter a day between 01 and 31.")
-                elif not (1 <= month <= 12):
-                    print("Invalid month. Please enter a month between 01 and 12.")
-                elif not (1900 <= year <= datetime.now().year):
-                    print(f"Invalid year. Please enter a year between 1900 and {datetime.now().year}.")
-                else:
-                    datetime.strptime(new_birthday, "%d/%m/%Y")
+                    print("This is already your current date of birth")
                     break
-            except Exception:
-                print(f"Invalid birth date, try again.")
-        account_details["date_of_birth"] = new_birthday
-        data["patient"][email_address] = account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your date of birth has been upodated successfully")
-        patients_page(email_address)
 
 
-    elif update_option == 5:
-        while True:
-            print("Genders:")
-            print("[ 1 ] Male")
-            print("[ 2 ] Female")
-            try:
-                new_gender = input("Please choose an option: ").strip()
-                if new_gender == "1":
+        elif update_option == 5:
+            while True:
+                print("Genders:")
+                print("[ 1 ] Male")
+                print("[ 2 ] Female")
+
+                new_gender_option = input("Please choose an option: ").strip()
+                if new_gender_option == "1":
                     new_gender = "Male"
-                    break
-                elif new_gender == "2":
+                elif new_gender_option == "2":
                     new_gender = "Female"
+                else:
+                    print("Please choose a valid option '1' or '2'")
+
+                if new_gender != account_details["gender"]:
+                    account_details["gender"] = new_gender
+                    updates_made= True
                     break
                 else:
                     print("Please choose a valid option '1' or '2'")
-            except Exception:
-                print("Invalid option")
-        account_details["gender"] = new_gender
-        data["patient"][email_address] = account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your gender has been upodated successfully")
-        patients_page(email_address)
-
-    elif update_option == 6:
-        while True:
-            print("NHS blood donor:")
-            print("[ 1 ] Yes")
-            print("[ 2 } No")
-            try:
-                a_new = input("Please choose an option: ").strip()
-                if a_new == "1":
-                    a_new = "IS Blood donor"
                     break
-                elif a_new == "2":
-                    a_new = "NOT Blood donor"
+
+
+        elif update_option == 6:
+            while True:
+                print("NHS blood donor:")
+                print("[ 1 ] Yes")
+                print("[ 2 } No")
+                donor_new = input("Please choose an option: ").strip()
+                if donor_new_option == "1":
+                    donor_new = "IS Blood donor"
+                elif donor_new_option == "2":
+                    donor_new = "NOT Blood donor"
+                else:
+                    print("Invalid choice, Please choose 1 or 2")
+                    continue
+
+                if donor_new != account_details["NHS_blood_donor"]:
+                    account_details["NHS_blood_donor"] = donor_new
+                    updates_made = True
                     break
                 else:
-                    print("Please choose '1', '2'")
-            except Exception:
-                print("Invalid option")
-        account_details["NHS_blood_donor"] = a_new
-        data["patient"][email_address] = account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your NHS blood donor status has been upodated successfully")
-        patients_page(email_address)
-
-    elif update_option == 7:
-
-        while True:
-            print("NHS organ donor:")
-            print("[ 1 ] Yes")
-            print("[ 2 } No")
-
-            try:
-                b_new = input("Please choose an option: ").strip()
-                if b_new == "1":
-                    b_new = "IS Organ donor"
+                    print("This is already your current NHS blood donor status")
                     break
-                elif b_new == "2":
-                    b_new = "NOT Organ donor"
+
+
+        elif update_option == 7:
+
+            while True:
+                print("NHS organ donor:")
+                print("[ 1 ] Yes")
+                print("[ 2 } No")
+                organ_new = input("Please choose an option: ").strip()
+                if organ_new == "1":
+                    organ_new = "IS Organ donor"
+                elif organ_new == "2":
+                    organ_new = "NOT Organ donor"
+                else:
+                    print("Invalid choice. Please choose 1 or 2")
+                    continue
+
+
+                if organ_new != account_details["NHS_organ_donor"]:
+                    account_details["NHS_organ_donor"] = organ_new
+                    updates_made= True
                     break
                 else:
-                    print("Please choose '1', '2' or 'M' to return to menu")
-            except Exception:
-                print("Invalid option")
-        account_details["NHS_organ_donor"] = b_new
-        data["patient"][email_address] = account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your NHS organ donor status has been upodated successfully")
-        patients_page(email_address)
-
-    elif update_option == 8:
-        while True:
-            try:
-                ad1_new = input("Address Line 1 (House number, street name): ")
-                address_line_1_pattern = r"^\d+\s+[\w\s\-']+(\s+(Apt|Suite|St|Rd|Lane|Blvd|Ave|Dr|Court|Pl|Road|Street|Way|Close))?(\s*\d*[A-Za-z]?)?$"
-                if match(address_line_1_pattern, ad1_new):
+                    print("This is already your current NHS organ donor status")
                     break
-                else:
-                    print("Address Line 1 is not valid.")
-            except Exception:
-                print("Invalid address, try again")
-        account_details["Address_Line_1"] = ad1_new
-        data["patient"][email_address] = account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your Address Line 1 has been upodated successfully")
-        patients_page(email_address)
 
-    elif update_option == 9:
-        while True:
-            try:
-                ad2_new = input("Address Line 2 (Postcode, zipcode, or country): ")
-                address_line_2_pattern = r'^[A-Za-z0-9\s,]+$'
-                if match(address_line_2_pattern, ad2_new):
-                    break
-                else:
-                    print("Address Line 1 is not valid.")
-            except Exception:
-                print("Invalid address, try again")
-        account_details["Address_Line_2"] = ad2_new
-        data["patient"][email_address] = account_details
-        with open("accounts.json", "w") as file:
-            dump(data, file, indent=4)
-        print("Your Address Line 2 has been upodated successfully")
-        patients_page(email_address)
+
+        elif update_option == 8:
+
+            ad1_new = input("Please enter a new Address Line 1 ")
+            if ad1_new != account_details["Address_Line_1"]:
+                account_details["Address_Line_1"] = ad1_new
+                updates_made= True
+            else:
+                print("This is already your current Address Line 2")
+
+
+        elif update_option == 9:
+            ad2_new = input("Please enter a new Address Line 2 ")
+            if ad2_new != account_details["Address_Line_2"]:
+                account_details["Address_Line_2"] = ad2_new
+                updates_made = True
+            else:
+                print("This is already your current Address Line 2")
+
+        else:
+            print("Invalid option")
+            continue
+
+
+
+        if updates_made:
+            data["patient"][email_address]= account_details
+            with open("accounts.json","w") as file:
+                dump(data , file ,indent=4)
+            print("Updating your account details...")
+            sleep(1)
+            print("Your changes have been saved successfully!")
+            sleep(1)
+            updates_made = False
+
+        else:
+            print("No changes were made.")
+            sleep(1)
+
+        print("--------------------------------------------------------------------------------")
+        print("Updated account details:")
+        print("")
+        for key,value in account_details.items():
+            if key!= "password":
+                print((key[0].upper()) + key[1:len(key)].replace("_", " "), ": ", value)
+
+        print("\n")
+        edit_again= input("Would you like to eit another detail? (Y/N): ").strip().lower()
+        if edit_again == "n":
+            print("Returning to your homepage...")
+            sleep(1)
+            patients_page(email_address)
+            break
+        elif edit_again!= "y":
+            print("Invalid input, returning to the edit menu")
+
+
 #==========================================================
 
 
